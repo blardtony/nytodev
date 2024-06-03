baa24aeb0c354f3526030a567c102bd55d1c2b50
GQBR5408 <tony.blard@orange.com>
2024-06-03 15:04:59 +0200

    feat : Import styles/buttons.css

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,6 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 import './styles/layout.css';
+import './styles/button.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
