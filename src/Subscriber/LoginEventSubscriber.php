aa5d4d792807abde8b110e9c8b43c02f2a2eb804
Tony Blard <tonyblard55@gmail.com>
2024-06-03 21:56:09 +0200

    feat : Create LoginEventSubscriber Handle event BadPasswordLoginEvent and LoginSuccessEvent

---
+++ src/Subscriber/LoginEventSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Subscriber;
+
+use App\Entity\User;
+use App\Event\BadPasswordLoginEvent;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+
+readonly class LoginEventSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BadPasswordLoginEvent::class => 'onAuthenticationFailure',
+            LoginSuccessEvent::class => 'onAuthenticationSuccess'
+        ];
+    }
+
+    public function onAuthenticationFailure(): void
+    {
+        dump('Event dispatched');
+    }
+
+    public function onAuthenticationSuccess(LoginSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+        if ($user instanceof User) {
+            $user->setLastLoginAt(new DateTimeImmutable());
+            $this->entityManager->flush();
+        }
+    }
+}
