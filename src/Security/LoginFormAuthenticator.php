98686f76680dbcbf1578d4293044cf46923747a6
Tony Blard <tonyblard55@gmail.com>
2024-06-03 22:04:31 +0200

    feat : Create LoginFormAuthenticator

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Event\BadPasswordLoginEvent;
+use App\Repository\UserRepository;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+{
+    private ?Passport $passport = null;
+    final public const string LOGIN_ROUTE = 'auth_login';
+    use TargetPathTrait;
+
+    public function __construct(private readonly UrlGeneratorInterface $urlGenerator, private readonly UserRepository $userRepository, private readonly UrlMatcherInterface $urlMatcher, private readonly EventDispatcherInterface $eventDispatcher)
+    {
+    }
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email', '');
+        $password = $request->request->get('password', '');
+
+        $this->passport = new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge('authenticate', $request->get('_csrf_token'))
+            ]
+        );
+        return $this->passport;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): RedirectResponse
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('home'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $user = $this->passport?->getUser();
+        if ($user instanceof User &&
+            $exception instanceof BadCredentialsException
+        ) {
+            $this->eventDispatcher->dispatch(new BadPasswordLoginEvent($user));
+        }
+        return parent::onAuthenticationFailure($request, $exception);
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
