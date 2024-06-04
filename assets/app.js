4a66b5954beab8cdf2e1f508fb4af947714cf34e
Tony Blard <tonyblard55@gmail.com>
2024-06-04 20:35:34 +0200

    feat : Import all css for auth-login

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,8 @@ import './bootstrap.js';
 import './styles/app.css';
 import './styles/layout.css';
 import './styles/button.css';
+import './styles/alert.css';
+import './styles/page/auth.css';
+import './styles/form.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
