20dbdd39ebee84f9e02afbef2565d3b5be74e52d
Tony Blard <tonyblard55@gmail.com>
2024-06-13 23:02:07 +0200

    feat : Create LoginFormAuthenticatorTest Test if LoginAuthenticator is working

---
+++ tests/Auth/LoginFormAuthenticatorTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Auth;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Security\LoginFormAuthenticator;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+
+class LoginFormAuthenticatorTest extends TestCase
+{
+
+    /**
+     * @var MockObject|UserRepository
+     */
+    private MockObject|UserRepository $userRepository;
+
+    /**
+     * @var MockObject|UrlGeneratorInterface
+     */
+    private MockObject|UrlGeneratorInterface $urlGenerator;
+
+    /**
+     * @var EventDispatcherInterface|MockObject
+     */
+    private MockObject|EventDispatcherInterface $eventDispatcher;
+
+    private LoginFormAuthenticator $authenticator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->userRepository = $this->getMockBuilder(UserRepository::class)
+            ->disableOriginalConstructor()->getMock();
+        $this->urlGenerator = $this->getMockBuilder(UrlGeneratorInterface::class)->getMock();
+        $this->eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+        /* @var MockObject|UrlMatcherInterface $urlMatcher */
+        $urlMatcher = $this->createMock(UrlMatcherInterface::class);
+        $urlMatcher->expects($this->any())->method('match')->willReturn([]);
+        $this->authenticator = new LoginFormAuthenticator(
+            $this->urlGenerator,
+            $this->userRepository,
+            $this->createMock(UrlMatcherInterface::class),
+            $this->eventDispatcher,
+        );
+    }
+    public function testPassportAuthentication(): void
+    {
+        $request = new Request([], ['email' => 'user@test.fr']);
+
+        $user = new User();
+        $this->userRepository
+            ->expects($this->once())
+            ->method('findOneByEmail')
+            ->with('user@test.fr')
+            ->willReturn(new User())
+        ;
+
+        $passport = $this->authenticator->authenticate($request);
+        $this->assertEquals($passport->getUser(), $user);
+        $this->assertTrue($passport->hasBadge(CsrfTokenBadge::class));
+        $this->assertTrue($passport->hasBadge(PasswordCredentials::class));
+    }
+}
