48ec23cf1a840fde8450e7fdf05eea390cee83ff
Tony Blard <tonyblard55@gmail.com>
2024-06-14 00:10:58 +0200

    feat : update LoginFormAuthenticatorTest Delete urlMatcher

--- tests/Auth/LoginFormAuthenticatorTest.php
+++ tests/Auth/LoginFormAuthenticatorTest.php
@@ -31,13 +31,9 @@ class LoginFormAuthenticatorTest extends TestCase
             ->disableOriginalConstructor()->getMock();
         $urlGenerator = $this->getMockBuilder(UrlGeneratorInterface::class)->getMock();
         $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
-        /* @var MockObject|UrlMatcherInterface $urlMatcher */
-        $urlMatcher = $this->createMock(UrlMatcherInterface::class);
-        $urlMatcher->expects($this->any())->method('match')->willReturn([]);
         $this->authenticator = new LoginFormAuthenticator(
             $urlGenerator,
             $this->userRepository,
-            $this->createMock(UrlMatcherInterface::class),
             $eventDispatcher,
         );
     }
