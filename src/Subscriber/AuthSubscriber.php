5cb0c80a6b14077b61b671c3e27fd8fc12ac183a
Tony Blard <tonyblard55@gmail.com>
2024-06-13 18:18:12 +0200

    feat : send email onRegister

--- src/Subscriber/AuthSubscriber.php
+++ src/Subscriber/AuthSubscriber.php
@@ -3,11 +3,13 @@
 namespace App\Subscriber;
 
 use App\Event\UserCreatedEvent;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
 
 readonly class AuthSubscriber implements EventSubscriberInterface
 {
-    public function __construct()
+    public function __construct(private MailerInterface $mailer)
     {
     }
     public static function getSubscribedEvents(): array
@@ -20,6 +22,15 @@ readonly class AuthSubscriber implements EventSubscriberInterface
     public function onRegister(UserCreatedEvent $event): void
     {
         $user = $event->getUser();
-        //TODO : Send email to user
+        $email = new TemplatedEmail();
+        $email
+            ->from('no-reply@nytodev.fr')
+            ->to($user->getEmail())
+            ->subject('Nytodev | Activation du compte')
+            ->htmlTemplate('emails/activate_account.html.twig')
+            ->context([
+                'user' => $user
+            ]);
+        $this->mailer->send($email);
     }
 }
