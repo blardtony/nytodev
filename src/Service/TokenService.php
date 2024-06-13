42270775c999ce3ea108860315b53ebb7601176d
Tony Blard <tonyblard55@gmail.com>
2024-06-14 00:19:48 +0200

    feat : Length must greater than 1

--- src/Service/TokenService.php
+++ src/Service/TokenService.php
@@ -6,6 +6,9 @@ class TokenService
 {
     public function generateToken(int $length = 32): string
     {
+        if ($length < 1) {
+            $length = 32;
+        }
         return bin2hex(random_bytes($length));
     }
 }
